000d4a32b265fc5caeed0e28f8051b96a78e005d
automobile <automobile@192.168.8.180>
2019-08-01 15:12:34 +0530

    updating

--- Mymobileproject/src/main/java/selenium/browser.java
+++ Mymobileproject/src/main/java/selenium/browser.java
@@ -3,10 +3,11 @@ package selenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class browser {
 	public void initiatebrowser() {
 
-	System.setProperty("webdriver.chrome.driver","/Users/automobile/Downloads/chromedriver");
+	System.setProperty("webdriver.chrome.driver","/chromedriver.exe");
 	WebDriver driver =new ChromeDriver();
 	driver.get("http://www.google.com");
 	System.out.println(driver.getTitle());

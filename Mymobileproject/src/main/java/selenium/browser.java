7a692b632048a95ebeffb2a7fa01d6d3dbba7ce6
kpramkiran <31835108+kpramkiran@users.noreply.github.com>
2019-08-04 13:11:13 +0530

    Update browser.java
    add new code

--- Mymobileproject/src/main/java/selenium/browser.java
+++ Mymobileproject/src/main/java/selenium/browser.java
@@ -7,10 +7,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class browser {
 	public void initiatebrowser() {
 
-	System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/chromedriver 2");
+	/*System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/chromedriver 2");
 	WebDriver driver =new ChromeDriver();
 	driver.get("http://www.google.com");
-	System.out.println(driver.getTitle());
+	System.out.println(driver.getTitle());*/
+	System.out.println("This is first project")
+		
 	
 }
 }
